398c08a0e1c2ef6975bb0b8922d66080700b84ce
SoumyajitBasu1988 <soumyajit.basu62@gmail.com>
2017-03-24 14:23:48 +0530

    Addin in changes to the repository

--- src/test/java/Docker_Selenium/dock/AppTest.java
+++ src/test/java/Docker_Selenium/dock/AppTest.java
@@ -3,12 +3,12 @@ package Docker_Selenium.dock;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
-
 import Docker_Selenium.pageobject.LogIn;
 import Docker_Selenium.pageobject.LogOut;
-import io.github.bonigarcia.wdm.ChromeDriverManager;
 
 public class AppTest
 {
@@ -17,16 +17,27 @@ public class AppTest
 	@Test
 	public void Test()
 	{
-		ChromeDriverManager.getInstance().setup();
-		driver = new ChromeDriver();
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setJavascriptEnabled(true);
+		caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"Driver/phantomjs");
+		caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[] {"--web-security=no", "--ignore-ssl-errors=yes"});
+		driver = new PhantomJSDriver(caps);
+		System.out.println(">>>Driver initiated");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
+		System.out.println(">>>Browser window maximize");
 		driver.get("http://zero.webappsecurity.com/login.html");
+		System.out.println(">>>URL triggered");
 		LogIn.zero_user(driver).sendKeys("username");
+		System.out.println(">>>Username entered");
 		LogIn.zero_pass(driver).sendKeys("password");
+		System.out.println(">>>Password entered");
 		LogIn.zero_submit(driver).click();
+		System.out.println(">>>Submit button clicked");
+		System.out.println(">>>Inside the system");
 		LogOut.click_dropdown(driver).click();
 		LogOut.click_logout(driver).click();
+		System.out.println(">>>Logged out of the system");
 		driver.quit();
 	}
 }
